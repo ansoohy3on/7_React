2f17fbf728f2fa770584f6fb4386e16fda2c6d2c
ash <dkstngus0615@gmail.com>
2023-09-15 19:40:45 +0900

    0915 시험공부

--- todo-list/src/Signup.js
+++ todo-list/src/Signup.js
@@ -8,8 +8,50 @@ const SignupContainer = () => {
     const [name, setName] = useState('');
     const [result, setResult] = useState('');
 
+    // 아이디 중복 검사
+    const [idValidation, setIdValidation] = useState(false);
+    // false -> 사용 불가
+    // true -> 사용 가능
+
+    const idCheck = (inputId) => {
+        // inputId : 입력한 아이디
+        setId(inputId); // id 변수에 입력받은 아이디 대입
+
+        // 4글자 미만 검사 X
+        if(inputId.trim().length < 4){
+            setIdValidation(false);
+            return;
+        }
+
+        fetch("/idCheck?id=" + inputId)
+        .then(resp => resp.text())
+        .then(result => {
+            console.log(`result : ${result}`);
+
+            console.log(typeof result);
+
+            // number 타입으로 parsing
+            if(Number(result) == 0){ // 중복 X -> 사용 가능
+                setIdValidation(true);
+                
+            } else { // 중복 O -> 사용 불가능
+                setIdValidation(false);
+
+            }
+        })
+        .catch(e => console.log(e));
+
+    }
+
     // 회원 가입 함수
     const signup = () => {
+
+        // 아이디가 사용 불가인 경우(짧거나 중복)
+        if(!idValidation){
+            alert("아이디를 다시 입력해 주세요.");
+            return;
+        }
+
         // 1. 비밀번호가 일치하지 않으면 (pw !== pwCheck)
         //    '비밀번호가 일치하지 않습니다.' alert로 출력 후 return
         if(pw !== pwCheck){
@@ -53,8 +95,12 @@ const SignupContainer = () => {
             <label>
                 ID : 
                 <input type='text'
-                    onChange={ e => {setId(e.target.value)} }
+                    onChange={ e => {
+                        //setId(e.target.value)
+                        idCheck(e.target.value)
+                    } }
                     value={id}
+                    className={idValidation ? '' : 'id-error'}
                 />
             </label>
             <label>
